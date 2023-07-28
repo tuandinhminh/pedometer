0e4cc5b26e530d532c5eebbb02aa0ef5afdc53a4
tuart.dinh <ext-tuart.dinh@spdigital.sg>
2023-07-28 14:00:28 +0700

    init commit

---
+++ momo/src/main/java/pedometer/momo/MomoApplication.java
@@ -0,0 +1,13 @@
+package pedometer.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoApplication.class, args);
+	}
+
+}
---
+++ momo/src/test/java/pedometer/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package pedometer.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
