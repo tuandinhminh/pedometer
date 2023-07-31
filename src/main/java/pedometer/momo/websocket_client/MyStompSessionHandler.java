348b697a8e09bdaf57812de800967363b592bc6f
tuart.dinh <ext-tuart.dinh@spdigital.sg>
2023-07-31 16:11:29 +0700

    add client test websocket

---
+++ src/main/java/pedometer/momo/websocket_client/MyStompSessionHandler.java
@@ -0,0 +1,40 @@
+package pedometer.momo.websocket_client;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+
+import java.lang.reflect.Type;
+
+public class MyStompSessionHandler extends StompSessionHandlerAdapter {
+
+    private final Logger logger = LogManager.getLogger(MyStompSessionHandler.class);
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+        logger.info("New session established : " + session.getSessionId());
+        session.subscribe("/topic/chart", this);
+        logger.info("Subscribed to /topic/chart");
+        session.send("/pedometer/chart", "Hello");
+        logger.info("Message sent to websocket server");
+    }
+
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+        logger.error("Got an exception", exception);
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return String.class;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        String msg = (String) payload;
+        logger.info("Received : " + msg);
+    }
+}
---
+++ src/main/java/pedometer/momo/websocket_client/StompClient.java
@@ -0,0 +1,27 @@
+package pedometer.momo.websocket_client;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.Scanner;
+
+public class StompClient {
+
+    private static String URL = "ws://localhost:3002/chart";
+
+    public static void main(String[] args) {
+        WebSocketClient client = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+
+        stompClient.setMessageConverter(new StringMessageConverter());
+
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        stompClient.connect(URL, sessionHandler);
+
+        new Scanner(System.in).nextLine(); // Don't close immediately.
+    }
+}
